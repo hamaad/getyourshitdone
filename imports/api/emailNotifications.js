4f4042138d2c3e53f9cd3306668b20f0bfa023e0
Hamaad Markhiani <hamaad.markhiani@gmail.com>
2017-12-05 12:58:54 -0600

    implemented contact us page functionality

--- imports/api/emailNotifications.js
+++ imports/api/emailNotifications.js
@@ -4,7 +4,8 @@ import { Email } from 'meteor/email';
 
 if (Meteor.isServer) {
   Meteor.methods({
-    'emailNotifications.sendEmail'(emailAddress, subject, message) { // sends an email to the passed email with the passed content
+    // for general email notifications
+    'emailNotifications.sendEmail'(emailAddress, subject, message) {
       Email.send({
         from: "notifications@getyourshitdone.club",
         to: emailAddress,
@@ -12,5 +13,16 @@ if (Meteor.isServer) {
         text: message
       });
     },
+
+    // for contact us page
+    'emailNotifications.sendContactUsEmail'(emailAddress, message) {
+      Email.send({
+        from: emailAddress,
+        to: "getyourshitdone4@gmail.com",
+        subject: "Contact Us -- New Submission",
+        text: message
+      });
+    },
+
   });
 }
--- imports/ui/options/contactUs.js
+++ imports/ui/options/contactUs.js
@@ -1,5 +1,19 @@
 import './contactUs.html';
 
-Template.profile.helpers({
-  //
+Template.contactUs.events({
+  'click #submitContactUsButton'(event) {
+    event.preventDefault();
+
+    emailAddress = Meteor.user().emailAddress;
+    message = document.getElementById('messageText').value;
+
+    Meteor.call('emailNotifications.sendContactUsEmail', emailAddress, message, function(error, result) {
+      if (error) {
+        document.getElementById('error-message').innerHTML = error;
+      } else {
+        document.getElementById('messageText').value = "";
+        document.getElementById('error-message').innerHTML = "Message sent!";
+      }
+    });
+  },
 });
